4a59bb4e53a7175d6032462878567ce7b9991feb
Tony <tonyrkovar@gmail.com>
2019-09-09 00:18:20 -0500

    set my pathing for all of special operator and number and then created the buttons and components for those 3 and imported them to app js

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, {useState} from "react";
+import {numbers} from '../../../data';
+import  NumberButton from './NumberButton';
 
 //import any components needed
 
@@ -6,11 +8,18 @@ import React from "react";
 
 const Numbers = () => {
   // STEP 2 - add the imported data to state
+  const [numberState, setNumberState] = useState(numbers);
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
-       it any props needed by the child component*/}
+       it any props needed by the child component*/
+       numbers.map( e => {
+         return <NumberButton props={e} />
+       })
+       }
     </div>
   );
 };
+
+export default Numbers;
--- src/components/ButtonComponents/SpecialButtons/SpecialButton.js
+++ src/components/ButtonComponents/SpecialButtons/SpecialButton.js
@@ -1,9 +1,13 @@
 import React from "react";
+import { specials } from "../../../data";
 
-const SpecialButton = () => {
+const SpecialButton = ({props}) => {
   return (
     <>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
+      <button>{props}</button>
     </>
   );
 };
+
+export default SpecialButton;
